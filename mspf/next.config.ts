7916490284000d2a6e5ebbafc59e8099f0f8edd8
Katie Sparr <sparr.katier@gmail.com>
2025-04-25 10:34:06 -0500

    Deleted output:export in nexty config

--- mspf/next.config.ts
+++ mspf/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
     turbopack: {
       resolveAlias: {
         canvas: './empty-module.ts', // Add this to resolve the canvas import
