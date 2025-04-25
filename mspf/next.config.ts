016bbfa62eab0f2ccd58960e5f2f6fbb198c8834
Katie Sparr <sparr.katier@gmail.com>
2025-04-25 10:49:34 -0500

    Chnge Build Config for Netlify

--- mspf/next.config.ts
+++ mspf/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
     turbopack: {
       resolveAlias: {
         canvas: './empty-module.ts', // Add this to resolve the canvas import
