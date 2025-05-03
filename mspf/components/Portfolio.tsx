ea087ef9134def1fb0b16f55ae34f9a7ec487e50
Katie Sparr <sparr.katier@gmail.com>
2025-05-03 16:42:45 -0500

    Changed sizing of portfolio text desc

--- mspf/components/Portfolio.tsx
+++ mspf/components/Portfolio.tsx
@@ -23,8 +23,8 @@ function Portfolio() {
     <div className="flex flex-col bg-gray-50 w-full items-center justify-items-center">
             <h1 className=" text-white font-bold text-7xl self-center my-10 mx-28">Projects</h1>
             
-            <h2 className="text-gray-20 text-4xl text-center m-10">The following tracks were fully engineered and produced by myself as part of my Music Industry Studies: </h2>
-            <h2 className="text-gray-20 text-4xl text-center mb-32 m-10">Senior Capstone Project at Lousisiana Tech University.</h2>
+            <h2 className="text-gray-20 text-2xl sm:text-xl md:text-2xl lg:text-4xl text-center m-10">The following tracks were fully engineered and produced by myself as part of my Music Industry Studies: </h2>
+            <h2 className="text-gray-20 text-2xl sm:text-xl md:text-2xl lg:text-4xl text-center mb-32 m-10">Senior Capstone Project at Lousisiana Tech University.</h2>
           
             { PORTFOLIO_PLAYLIST.map((song, index) => {
                 const isVisible = visibleItems[index];
